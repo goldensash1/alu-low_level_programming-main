05142676ce565b56d20aa1a56ca8ec441847a90f
Sash Golden Munyankindi <s.munyankin@alustudent.com>
2026-01-30 20:45:33 +0200

    final commit

---
+++ hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, msg, 59);
+return (1);
+}
